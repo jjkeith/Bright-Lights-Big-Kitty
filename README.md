5826faaeb19eee7fc23f9930561a6413c7e50973
JJ <jj@jjkeith.net>
2016-05-16 16:16:19 -0700

    Began writing functions to power the game; added bootstrap

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 #Hairballerz
-The goal is make some cat cash (ξ) by building establishments early in the game. As players accrue cash, they can start investing in destination properties that will help them make more money on their establishments. In order to win, player must buy all the destinations in order until they get the WCAT Television Studio and win the game.####Inspiration
+* [include link to finished game] 
+
+The goal is earn enough money on establishments to buy the WCAT Television Station.####Inspiration
 After playing Machi Koro, a Japanese card game where players act as mayors and invest in infrastructure, my  seven-year-old daughter wanted to create a similar game, but with her favorite animal: cats. 
 
 Together, we created new rules for gameplay that both simplified the game for younger players and sped up game play. She turned in a paper version of the game for a school project (then named "Cat Race to the Fashion Show") and I set about making a web-based version with a new title and some new cards. ####Methods
 
 My original plan for laying out the screen was to put player info at the bottom of the page as below. However, I switched it around because I felt that having that information at the top was more intuitive. 
 
-![](Wireframe.png)###Game Requirements
-
-
-###Instructions#####Cat Establishments* Can have up to two of each
+![](Wireframe.png)
+[image will appear once game is uploaded]###Instructions#####Cat Establishments* Can have up to two of each
 * Can only buy when you roll that number
 * When a player obtains a destination, the card changes to the player's color.1. The Veterinary Offices of Katz & Nuzzle - Costs 1ξ and earns 1ξ when a 1 is rolled.2. Tuna 'R' Us — Costs 2ξ and earns 2ξ when a 2 is rolled3. Purrfect Coif Groomer — Costs 3ξ and earns 3ξ when a 3 is rolled4. Clawsitive Reinforcement Training Academy — Costs 4ξ and earns 4ξ when a 4 is rolled5. Pawsplay Costume Rentals — Costs 5ξ and earns 5ξ when a 5 is rolled6. Flea Strasburg Acting School — Costs 6ξ and earns 6ξ when a 6 is rolled#####Cat Destinations* Can only have one of each* They have to be bought in order
 * * When a player obtains a development, the card changes to the player's color.1. Catnip Dispensary - Costs 10ξ, earns 1ξ extra on The Veterinary Offices of Katz & Nuzzle 2. Tuna Cannery - Costs 15ξ, earns 1ξ extra on the Tuna 'R' Us3. Catapult Magazine Headquarters - Costs 20ξ, earns 1ξ extra on Purrfect Coif Groomer4. Paw-trait Studio - Costs 25ξ, earns 1ξ extra on Clawsitive Reinforcement Training Academy5. CatCon Venue- Costs 30ξ, earns 1ξ extra on Pawsplay Costume Rentals6. WCAT Television Station - Costs 35ξ#####On Each Roll1. Both players collect rent on any cards held.2. The active player can choose to buy whatever he or she can afford.
@@ -20,5 +20,4 @@ Hairballerz was created with HTML, CSS, Javascript and jQuery.
 * Rules and theme: Beatrix Keith
 * Inspiration: Machi Koro © IDW Games, 2015##Future Plans
 ####Starter cards######four cards total; chosen at random
->Note that I should start building this with just one card and everyone collects rent on every turn
-1. Cat Rescue - odds2. Cat Breeder - evens####Computer DecisionsProbably default to the computer buying any property it can afford. 
+1. Cat Rescue - odds2. Cat Breeder - evens####Computer DecisionsProbably default to the computer buying any property it can afford. 
--- app.js
+++ app.js
@@ -1,18 +1,113 @@
-game = {
-    {player1,
-      balance: 0
-    },
-    {player2,
-      balance: 0
-    },
+var game = {
+  // player object; start each player with 4 cat coins
+  player1: {
+    id: 'Player 1',
+    balance: 4,
+    color: '#448888'},
+  player2: {
+    id: 'Player 2',
+    balance: 4,
+    color: '#b1d46a'},
+
+    // button variables
     start: $('#start'),
     reset: $('#reset'),
+    roll: $('#roll'),
+
+    //DOM variables. Need to add a bunch!
     $h1: $('<h1>'),
-    $cards: $('.card'),
-    est1a: $('#est1a'),
-    est1b: $('#est1b'),
-    est2a: $('#est2a'),
-    est2b: $('#est2b'),
-    est3a: $('#est3a'),
-    est3b: $('#est3b'),
+    estA1: $('#estA1'),
+
+    establishmentCardsFront: ['', $('#estA1'), $('#estA2'), $('#estA3'), $('#estA4'), $('#estA5') $('#estA6')],
+
+    //gameplay variables
+    currentPlayer: null,
+    currentRoll: null,
+
+    //dialog string variables
+    strings: ['', "The Veterinary Offices of Katz & Nuzzle for ξ1",
+      "Tuna 'R' Us for ξ2",
+      "Purrfect Coif Groomer for ξ3",
+      "Clawsitive Reinforcement Training Academy for ξ4",
+      "Pawsplay Costume Rentals for ξ5",
+      "Flea Strasburg Acting School for ξ6"],
 }
+
+
+//Activate the start button and randomize a starting player
+game.start.click(function() {
+  console.log("start")
+  var random = Math.random();
+  if (random < .5) {
+    game.currentPlayer = game.player1;
+    console.log("Player 1's turn");
+  } else {
+    game.currentPlayer = game.player2;
+    console.log("Player 2's turn");
+  }
+  return game.currentPlayer
+})
+
+//activate the roll button
+game.roll.click(function(){
+  //make the roll button work if start hasn't been pushed
+  if (game.currentPlayer == null) {
+    game.currentPlayer = game.player1
+  }
+  game.currentRoll = (Math.ceil(Math.random() * 6));
+  console.log(game.currentPlayer.id + " rolled a " + game.currentRoll);
+  $(".modal-title").html('');
+  $(".modal-title").html(game.currentPlayer.id + ' rolled a ' + game.currentRoll);
+  $(".modal-body").html('');
+  $(".modal-footer").html('');
+  if (game.currentPlayer.balance >= game.currentRoll) {
+    $(".modal-body").html('You have ' + game.currentPlayer.balance +
+      'ξ in the bank.<br />' + 'Would you like to buy ' +
+      game.strings[game.currentRoll] + '?');
+    $(".modal-footer").html('');
+//Can this be done better with a toggle class?
+    $(".modal-footer").html("<button type='button' class='btn btn-default' data-dismiss='modal'>No</button><button type='button' class='btn btn-primary' data-dismiss='modal' id = 'yes'>Yes</button>");
+    $('#yes').click(function() {
+      game.currentPlayer.balance -= game.currentRoll;
+      if estA[game.currentRoll].hasClass('unpurchased')) {
+        estA[game.currentRoll].toggleClass('purchased');
+        estA[game.currentRoll].css("color", game.currentPlayer.color)
+    }
+      console.log(game.currentPlayer.id + ' has ξ' + game.currentPlayer.balance + ' in the bank.')
+
+      })
+  } else {
+      // $(".modal-title").html('');
+      // $(".modal-title").html('You rolled a ' + game.currentRoll);
+      $(".modal-body").html('You have ' + game.currentPlayer.balance + 'ξ in the bank.<br />' + 'Sorry, that is not enough to buy ' + game.strings[game.currentRoll]);
+      console.log(game.currentPlayer.id + ' has ξ' + game.currentPlayer.balance + ' in the bank.')
+    }
+    switchTurns ();
+  })
+
+
+
+//switch statement
+//associate the rolls with cards
+// var assignEstablishments = function () {
+//   if (game.currentRoll == 1) {
+//     alert("Would you like to buy The Veterinary Offices of Katz & Nuzzle for ξ1?");
+//       if
+//   }
+
+// }
+
+// Switch players
+var switchTurns = function () {
+if (game.currentPlayer == game.player1) {
+  game.currentPlayer = game.player2;
+} else {
+  game.currentPlayer = game.player1;
+}
+}
+
+//Activate the reset button
+game.reset.click(function() {
+  console.log("reset")
+  currentPlayer = null;
+})
