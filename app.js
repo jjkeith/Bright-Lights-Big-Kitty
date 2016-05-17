0256818909f9a4cbf80897cd855ab8c99ab841e9
JJ <jj@jjkeith.net>
2016-05-17 10:21:05 -0700

    switchTurns is now working; still buggy

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
-//Hey, me. What you need to do now is set up the mechanism for EARNING money.
-//You also need to work on displaying the balance on the page.
+//Hey, me. What you need to do now is fix the mechanism for EARNING money.
 //Once you get that shit done, you need to deal with destinations, including
 //asking every player with more than 10 coins if they want to buy.
 
 var game = {
-  // player object; start each player with 4 cat coins
+  // player object; start each player with 10 cat coins
   player1: {
     id: 'Player 1',
-    balance: 4,
-    color: '#448888'},
+    balance: 10,
+    color: '#448888',
+    balanceDisplay: $('.player1BalanceDisplay'),
+    announce: $('#player1Announce'),
+  },
   player2: {
     id: 'Player 2',
-    balance: 4,
-    color: '#b1d46a'},
+    balance: 10,
+    color: '#b1d46a',
+    balanceDisplay: $('.player2BalanceDisplay'),
+    announce: $('#player2Announce'),
+  },
 
     // button variables
     start: $('#start'),
@@ -21,15 +26,16 @@ var game = {
 
     //DOM variables.
     $h1: $('<h1>'),
-    activeProperty: null,
-    player1BalanceDisplay: $('.player1BalanceDisplay'),
-    player2BalanceDisplay: $('.player2BalanceDisplay'),
-    player1Announce: $('#player1Announce'),
-    player2Announce: $('#player2Announce'),
+
+    // player1BalanceDisplay: $('.player1BalanceDisplay'),
+    // player2BalanceDisplay: $('.player2BalanceDisplay'),
+    // player1Announce: $('#player1Announce'), // turns yellow when it's player 1's turn
+    // player2Announce: $('#player2Announce'), // turns yellow when it's player 2's turn
 
     //gameplay variables
     currentPlayer: null,
     currentRoll: null,
+    activeProperty: null,
 
     //dialog string variables
     strings: ["The Veterinary Offices of Katz & Nuzzle for ξ1",
@@ -40,8 +46,11 @@ var game = {
       "Flea Strasburg Acting School for ξ6"],
 }
 //display balances at start of game
-game.player1BalanceDisplay.html('ξ' + game.player1.balance);
-game.player2BalanceDisplay.html('ξ' + game.player2.balance);
+function  updateBalances() {
+  game.player1.balanceDisplay.html('ξ' + game.player1.balance);
+  game.player2.balanceDisplay.html('ξ' + game.player2.balance);
+}
+updateBalances();
 
 //Activate the start button and randomize a starting player
 game.start.click(function() {
@@ -68,19 +77,11 @@ game.roll.click(function(){
     game.currentPlayer = game.player1
   }
   //create a die roll from 0 to 5
-  game.currentRoll = (Math.floor(Math.random() * 5));
-
-  //console.log the real roll
-  console.log(game.currentPlayer.id + " rolled a " + game.currentRoll);
-  $(".modal-title").html('');
+  game.currentRoll = (Math.floor(Math.random() * 6));
 
   //give the user a normalized 1 through 6 die roll
   $(".modal-title").html(game.currentPlayer.id + ' rolled a ' +
     (game.currentRoll + 1));
-
-  //strip the body and footer html out of the modal
-  $(".modal-body").html('');
-  $(".modal-footer").html('');
 
   //define the active property
   game.activeProperty = $('[data-number=' + game.currentRoll +']')
@@ -91,29 +92,7 @@ game.roll.click(function(){
   if (game.currentPlayer.balance >= (game.currentRoll + 1)) {
     if (((game.activeProperty.first()).hasClass('unpurchased'))
       || ((game.activeProperty.last()).hasClass('unpurchased'))) {
-        $(".modal-body").html('You have ξ' + game.currentPlayer.balance +
-          ' in the bank.<br />' + 'Would you like to buy ' +
-          game.strings[game.currentRoll] + '?');
-
-        //Can this be done better with a toggle class?
-        $(".modal-footer").html("<button type='button' class='btn btn-default' data-dismiss='modal'>No</button><button type='button' class='btn btn-primary' data-dismiss='modal' id = 'yes'>Yes</button>");
-
-        //Take the steps for the player to buy the property
-        $('#yes').click(function() {
-          game.currentPlayer.balance -= (game.currentRoll + 1);
-
-          //Use the classes to mark properties as purchased and which player owns it
-          //This code seems really chunky. There must be a better way.
-          if ((game.activeProperty.first()).hasClass('purchased')) {
-              (game.activeProperty.last()).css("background-color", game.currentPlayer.color);
-              (game.activeProperty.last()).removeClass('unpurchased');
-              (game.activeProperty.last()).toggleClass('purchased' + "'" + game.currentPlayer + "'");
-          } else {
-              (game.activeProperty.first()).css("background-color", game.currentPlayer.color);
-              (game.activeProperty.first()).removeClass('unpurchased');
-              (game.activeProperty.first()).toggleClass('purchased' + "'" + game.currentPlayer + "'");
-          }
-        })
+        purchaseOption(game.currentPlayer);
       } else {
           $(".modal-body").html('Both of the properties for this roll have been purchased.')
       }
@@ -134,29 +113,54 @@ game.roll.click(function(){
       console.log('Player 2 now has ξ' + game.player2.balance)
     }
 
-    //display updated balances
-    //I'd really like the balances to flash yellow when changed
-    game.player1BalanceDisplay.html('ξ' + game.player1.balance);
-    game.player2BalanceDisplay.html('ξ' + game.player2.balance);
-
-    console.log(game.currentPlayer.id + ' has ξ' + game.currentPlayer.balance + ' in the bank.')
+    updateBalances();
+    console.log(switchTurns)
     switchTurns ();
 })
 
-
 // Switch players, switch active player to yellow
-var switchTurns = function () {
+function  switchTurns () {
   if (game.currentPlayer == game.player1) {
     game.currentPlayer = game.player2;
-    game.player2Announce.css('color', 'yellow');
-    game.player1Announce.css('color', 'black');
+    game.player2.announce.css('color', 'yellow');
+    game.player1.announce.css('color', 'black');
   } else {
     game.currentPlayer = game.player1;
-    game.player1Announce.css('color', 'yellow');
-    game.player2Announce.css('color', '#121212');
+    game.player1.announce.css('color', 'yellow');
+    game.player2.announce.css('color', '#121212');
   }
 }
 
+function purchaseOption(player) {
+  $(".modal-body").html('You have ξ' + player.balance +
+    ' in the bank.<br />' + 'Would you like to buy ' +
+    game.strings[game.currentRoll] + '?');
+
+  //Can this be done better with a toggle class?
+  // $(".modal-footer").html("<button type='button' class='btn btn-default' data-dismiss='modal'>No</button><button type='button' class='btn btn-primary' data-dismiss='modal' id = 'yes'>Yes</button>");
+
+  //Take the steps for the player to buy the property
+  $('#yes').on('click', {player: player} ,function(evt) {
+    console.log(evt.data);
+    console.log("Entering yes function. Current player is " + evt.data.player.id)
+
+    evt.data.player.balance -= (game.currentRoll + 1);
+
+    //Use the classes to mark properties as purchased and which player owns it
+    //This code seems really chunky. There must be a better way.
+    if ((game.activeProperty.first()).hasClass('purchased')) {
+        (game.activeProperty.last()).css("background-color", evt.data.player.color);
+        (game.activeProperty.last()).removeClass('unpurchased');
+        (game.activeProperty.last()).addClass('purchased' + "'" + evt.data.player + "'");
+    } else {
+        (game.activeProperty.first()).css("background-color", evt.data.player.color);
+        (game.activeProperty.first()).removeClass('unpurchased');
+        (game.activeProperty.first()).addClass('purchased' + "'" + evt.data.player + "'");
+    }
+    evt.data.player.balanceDisplay.html('ξ' + evt.data.player.balance);
+
+  });
+}
 //Activate the reset button
 game.reset.click(function() {
   console.log("reset")
